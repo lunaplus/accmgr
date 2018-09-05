bada218928ad07f38b86ea2c4e46c762dad36c44
lunaplus <lunatic_redeyes@yahoo.co.jp>
2018-09-05 23:41:50 +0900

    bugfix: for iOS9(closure)

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener("DOMContentLoaded", function() {
     var _UA = navigator.userAgent;
     var s = document.createElement("script");
     s.type = "text/javascript";
-    if (_UA.search('iPhone') > -1){
+    if (_UA.search('iPhone') > -1
+	|| _UA.search('iPod') > -1
+	|| _UA.search('iPad') > -1
+       ){
 	s.src = "../js/main_legacy.js?v=" + Date.now();
     } else {
 	s.src = "../js/main_edge.js?v=" + Date.now();
     }
-
+    
     document.getElementsByTagName("head")[0].appendChild(s);
 }, false);
